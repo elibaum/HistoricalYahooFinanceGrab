62f7fc5189e0aee137ae188b885cd09504bf8c27
Eli Baum <elibaum@Eli.local>
2016-04-13 23:03:55 +0800

    Version 0.1

---
+++ Module.py
@@ -0,0 +1,103 @@
+from urllib import urlencode
+from urllib import urlretrieve
+
+import os
+import csv
+#Date,Open,High,Low,Close,Volume,Adj Close
+class Share(object):
+
+    def __init__(self, stock,start,(finish)):
+        self.quote =stock
+        self.start=start
+        self.finish=finish
+        self.params = urlencode({
+        's': self.quote,
+        'a': int(start[5:7]) - 1,
+        'b': int(start[8:10]),
+        'c': int(start[0:4]),
+        'd': int(finish[5:7]) - 1,
+        'e': int(finish[8:10]),
+        'f': int(finish[0:4]),
+        'g': 'd',
+        'ignore': '.csv',
+        })
+
+    def getopen(self):
+        try:
+            url = 'http://real-chart.finance.yahoo.com/table.csv?%s' % self.params
+            urlretrieve(url, '%s.csv' % self.quote)
+            with open("%s.csv" % self.quote,'rb') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    print row['Open'] +' '+ row['Date']
+            os.remove('%s.csv' % self.quote)
+        except:
+            cleanup(self.quote)
+
+    def gethigh(self):
+        try:
+            url = 'http://real-chart.finance.yahoo.com/table.csv?%s' % self.params
+            urlretrieve(url, '%s.csv' % self.quote)
+            with open("%s.csv" % self.quote,'rb') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    print row['High'] +' '+ row['Date']
+            os.remove('%s.csv' % self.quote)
+        except:
+            cleanup(self.quote)
+
+    def getlow(self):
+        try:
+            url = 'http://real-chart.finance.yahoo.com/table.csv?%s' % self.params
+            urlretrieve(url, '%s.csv' % self.quote)
+            with open("%s.csv" % self.quote,'rb') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    print row['Low'] +' '+ row['Date']
+            os.remove('%s.csv' % self.quote)
+        except:
+            cleanup(self.quote)
+
+
+    def getclose(self):
+        try:
+            url = 'http://real-chart.finance.yahoo.com/table.csv?%s' % self.params
+            urlretrieve(url, '%s.csv' % self.quote)
+            with open("%s.csv" % self.quote,'rb') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    print row['Close'] +' '+ row['Date']
+            os.remove('%s.csv' % self.quote)
+        except:
+            cleanup(self.quote)
+
+
+    def getvolume(self):
+        try:
+            url = 'http://real-chart.finance.yahoo.com/table.csv?%s' % self.params
+            urlretrieve(url, '%s.csv' % self.quote)
+            with open("%s.csv" % self.quote,'rb') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    print row['Volume'] +' '+ row['Date']
+            os.remove('%s.csv' % self.quote)
+        except:
+            cleanup(self.quote)
+
+
+    def getadj(self):
+        try:
+            url = 'http://real-chart.finance.yahoo.com/table.csv?%s' % self.params
+            urlretrieve(url, '%s.csv' % self.quote)
+            with open("%s.csv" % self.quote,'rb') as csvfile:
+                reader = csv.DictReader(csvfile)
+                for row in reader:
+                    print row['Adj Close'] +' '+ row['Date']
+            os.remove('%s.csv' % self.quote)
+        except:
+            cleanup(self.quote)
+
+def cleanup(quote):
+        print "Error"
+        try: os.remove('%s.csv' % quote)
+        except: pass
--- main.py
+++ main.py
@@ -1,32 +1,15 @@
-import urllib
-import os
-import csv
-from openpyxl import *
-quotes = ["601988.SS","600028.SS"]
-for x in range(len(quotes)):
-    quote = quotes[x]
-    urllib.urlretrieve ("http://real-chart.finance.yahoo.com/table.csv?s=%s&d=3&e=10&f=2016&g=d&a=6&b=5&c=2014&ignore=.csv" % str(quote),"%s.csv" % str(quote))
-    with open("%s.csv"%quote,'rb') as csvfile:
 
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            print row['Open'] +" " +row['Date']
-os.remove('%s.csv' % quote)
-
-"""
-wb =load_workbook('/Users/elibaum/Desktop/Testshares.xlsx')
-ws = wb.active
-with open("%s.csv"%quote,'rb') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for row in reader:
-        print row['Open'] +" " +row['Date']
-        for x in range(1,23):
-            tested = 'C' +str(x)
-            if str(ws[tested].value) == 'None':
-                ws[tested] = row['Open']
-                tempdate= row['Date']
-                ws['D'+str(x)] =tempdate
-                break
-wb.save('/Users/elibaum/Desktop/Testshares.xlsx')
-os.remove('%s.csv' % quote)
-"""
+from datetime import date
+today = date.today()
+print today
+print today
+import Module
+share = Module.Share("600028.SS",'2015-01-01',str(today))
+print share.getopen()
+'''
+print share.gethigh()
+print share.getlow()
+print share.getclose()
+print share.getvolume()
+print share.getadj()
+'''
